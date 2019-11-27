f061fc6c1139b25c48662734a73f96eee7af36d8
phil.lee <phil.lee@buzzvil.com>
2019-11-27 14:47:06 +0900

    visualize buildings

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,18 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+d3.json('data/buildings.json').then(data => {
+    const svg = d3.select("#chart-area").append('svg')
+        .attr("width", 500)
+        .attr("height", 500);
+    const rects = svg.selectAll("rect").data(data);
+
+    rects.enter()
+        .append("rect")
+        .attr("x", (d, i) => i * 30 + 20)
+        .attr("y", 20)
+        .attr("width", 20)
+        .attr("height", d => Number(d.height))
+        .attr("fill", 'grey')
+
+})
